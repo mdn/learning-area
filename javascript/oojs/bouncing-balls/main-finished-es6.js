1452c98bff1361836bd1df1d5cd8552fec322abd
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-05-09 09:21:36 -0400

    Removed duplicate lines and updated syntax
    Removed duplicate lines /variable declarations and updated `var` keyword to `let`. 
    Updated `random()` function to use arrow function syntax. 
    Also modified string concatenated lines to template literal syntax for added clarity.

--- javascript/oojs/bouncing-balls/main-finished-es6.js
+++ javascript/oojs/bouncing-balls/main-finished-es6.js
@@ -1,109 +1,97 @@
 // setup canvas
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
 
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
 // function to generate random number
-
-function random(min,max) {
-   var num = Math.floor(Math.random()*(max-min)) + min;
-   return num;
-}
-
-var canvas = document.querySelector('canvas');
-
-var ctx = canvas.getContext('2d');
-
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 class Ball {
-   
-   constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
-   }
-   
-   draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fill();
-   }
-   
-   update() {      
-      if ((this.x + this.size) >= width) {
-         this.velX = -(this.velX);
-      }
 
-      if ((this.x - this.size) <= 0) {
-         this.velX = -(this.velX);
-      }
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
 
-      if ((this.y + this.size) >= height) {
-         this.velY = -(this.velY);
-      }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 
-      if ((this.y - this.size) <= 0) {
-         this.velY = -(this.velY);
-      }
+    update() {
+        if ((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
 
-      this.x += this.velX;
-      this.y += this.velY;      
-   }
-   
-   collisionDetect() {     
-      for (var j = 0; j < balls.length; j++) {
-         if (!(this === balls[j])) {
-            var dx = this.x - balls[j].x;
-            var dy = this.y - balls[j].y;
-            var distance = Math.sqrt(dx * dx + dy * dy);
+        if ((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
 
-            if (distance < this.size + balls[j].size) {
-              balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        if ((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect() {
+        for (let j = 0; j < balls.length; j++) {
+            if (!(this === balls[j])) {
+                let dx = this.x - balls[j].x;
+                let dy = this.y - balls[j].y;
+                let distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    balls[j].color = this.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+                }
             }
-         }
-      }
-   }
-   
+        }
+    }
+
 }
 
-var balls = [];
+let balls = [];
 
 while (balls.length < 25) {
-   var size = random(10,20);
-   var ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
-      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-      size
-   );
+    let size = random(10, 20);
+    let ball = new Ball(
+        // ball position always drawn at least one ball width
+        // away from the edge of the canvas, to avoid drawing errors
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`,
+        size
+    );
 
-  balls.push(ball);
+    balls.push(ball);
 }
 
 function loop() {
-   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-   ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
 
-   for (var i = 0; i < balls.length; i++) {
-     balls[i].draw();
-     balls[i].update();
-     balls[i].collisionDetect();
-   }
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
 
-   requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
 }
 
 loop();
