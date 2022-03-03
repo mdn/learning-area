7d141439b5a1913af3e24ee83bdf6f2c339f4794
Will Bamberg <will@bootbonnet.ca>
2022-03-03 14:40:00 -0800

    Update sw code, 2

--- javascript/apis/client-side-storage/cache-sw/video-store-offline/sw.js
+++ javascript/apis/client-side-storage/cache-sw/video-store-offline/sw.js
@@ -14,8 +14,6 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
