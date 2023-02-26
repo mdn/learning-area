7830a4c6a0fc5780bb3b85cb78a01a1a75d8eb8d
Norman <nhb_jr@yahoo.com>
2023-02-27 01:50:12 +0800

    My first commit

--- javascript/introduction-to-js-1/assessment-finished/main1.js
+++ javascript/introduction-to-js-1/assessment-finished/main1.js
@@ -1,20 +1,36 @@
+const randomit = document.querySelector(".randomit");
+
+const enterName = document.getElementById("enterName");
+
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
 const storyText =
-  'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — "insertx" weighs 300 pounds, and it was a hot day.';
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertX = ["David the Handsom", "Charles the Brave"];
+const insertX = [
+  "David the Handsom",
+  "Charles the Brave",
+  "Honda the baseball hero",
+];
 
-const insertY = ["Dark place", "Humid forest"];
+const insertY = ["Dark place", "Humid forest", "Swamp of sexy girls"];
 
 const insertZ = [
   "running like a Wild horse even they",
   "slowly moving like a snail eating an ice cream that",
+  "walking while their footstep smoke in dust even",
 ];
 
 //when click it provides the result
-random.addEventListener("click", result);
+randomit.addEventListener("click", result);
 
 function result() {
-  const newStory = storyText;
+  let newStory = storyText;
 
   //For each one they basically have to initialise the new variable, and declare it's value as the corresponding array passed to the randomValueFromArray() function
   const xItem = randomValueFromArray(insertX);
@@ -25,15 +41,17 @@ function result() {
 
   newStory = newStory.replaceAll(":insertx:", xItem); //replace the placeholder with xItem
 
-  newStory = newStory.replaceAll(":insertY:", yItem); //replace the placeholder with yItem
+  newStory = newStory.replaceAll(":inserty:", yItem); //replace the placeholder with yItem
 
-  newStory = newStory.replaceAll(":insertZ:", zItem); //replace the placeholder with zItem
+  newStory = newStory.replaceAll(":insertz:", zItem); //replace the placeholder with zItem
 
-  //this line needs to be called twice if you use replace()
-}
+  //this line needs to be called twice if values is called
+  if (enterName.value !== "") {
+    const name = enterName.value;
+    newStory = newStory.replaceAll("Bob", name);
+  }
 
-function myFunction() {
-  if (uk == true) {
+  if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300 * 0.0714286)} stone`;
     const temperature = `${Math.round(((94 - 32) * 5) / 9)} centigrade`;
     newStory = newStory.replaceAll("94 fahrenheit", temperature); //replace the 94 fahrenheit with temperature value
