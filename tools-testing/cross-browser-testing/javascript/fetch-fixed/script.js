b28de6ab803237a8ccd7b400e7e2953df117086f
wbamberg <will@bootbonnet.ca>
2023-12-06 22:57:57 -0800

    Variable name changes
    Co-authored-by: Jean-Yves Perrier <jypenator@gmail.com>

--- tools-testing/cross-browser-testing/javascript/fetch-fixed/script.js
+++ tools-testing/cross-browser-testing/javascript/fetch-fixed/script.js
@@ -12,44 +12,44 @@ fetch(requestURL)
   });
 
 function populateHeader(jsonObj) {
-  const myH1 = document.createElement("h1");
-  myH1.textContent = jsonObj["squadName"];
-  header.appendChild(myH1);
+  const h1 = document.createElement("h1");
+  h1.textContent = jsonObj["squadName"];
+  header.appendChild(h1);
 
-  const myPara = document.createElement("p");
-  myPara.textContent = `Hometown: ${jsonObj["homeTown"]} // Formed: ${jsonObj["formed"]}`;
-  header.appendChild(myPara);
+  const para = document.createElement("p");
+  para.textContent = `Hometown: ${jsonObj["homeTown"]} // Formed: ${jsonObj["formed"]}`;
+  header.appendChild(para);
 }
 
 function showHeroes(jsonObj) {
   const heroes = jsonObj["members"];
 
   for (const hero of heroes) {
-    const myArticle = document.createElement("article");
-    const myH2 = document.createElement("h2");
-    const myPara1 = document.createElement("p");
-    const myPara2 = document.createElement("p");
-    const myPara3 = document.createElement("p");
-    const myList = document.createElement("ul");
+    const article = document.createElement("article");
+    const h2 = document.createElement("h2");
+    const para1 = document.createElement("p");
+    const para2 = document.createElement("p");
+    const para3 = document.createElement("p");
+    const list = document.createElement("ul");
 
-    myH2.textContent = hero.name;
-    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
-    myPara2.textContent = `Age: ${hero.age}`;
-    myPara3.textContent = "Superpowers:";
+    h2.textContent = hero.name;
+    para1.textContent = `Secret identity: ${hero.secretIdentity}`;
+    para2.textContent = `Age: ${hero.age}`;
+    para3.textContent = "Superpowers:";
 
     const superPowers = hero.powers;
     for (const superPower of superPowers) {
       const listItem = document.createElement("li");
       listItem.textContent = superPower;
-      myList.appendChild(listItem);
+      list.appendChild(listItem);
     }
 
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myList);
+    article.appendChild(h2);
+    article.appendChild(para1);
+    article.appendChild(para2);
+    article.appendChild(para3);
+    article.appendChild(list);
 
-    section.appendChild(myArticle);
+    section.appendChild(article);
   }
 }
